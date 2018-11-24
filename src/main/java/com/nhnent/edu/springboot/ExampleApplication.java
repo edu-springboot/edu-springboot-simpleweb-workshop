36b72460a09aed15acb388dadf721801471cd997
정지범 <jibum.jung@nhnent.com>
2018-11-24 21:45:12 +0900

    add jsp example

--- src/main/java/com/nhnent/edu/springboot/ExampleApplication.java
+++ src/main/java/com/nhnent/edu/springboot/ExampleApplication.java
@@ -2,15 +2,22 @@ package com.nhnent.edu.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class ExampleApplication {
+public class ExampleApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ExampleApplication.class);
     }
 
     @GetMapping("/hello")
---
+++ src/main/java/com/nhnent/edu/springboot/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.nhnent.edu.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", "Welcome to the world!");
+        return "welcome";
+    }
+}
