bd5099dc207b31c2336f8fc6d13d94b1114774fc
정지범 <jibum.jung@nhnent.com>
2019-04-02 14:57:39 +0900

    spring boot version 2.1.3, jsp debug option

--- src/main/java/com/nhnent/edu/springboot/ExampleApplication.java
+++ src/main/java/com/nhnent/edu/springboot/ExampleApplication.java
@@ -3,7 +3,7 @@ package com.nhnent.edu.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -5,6 +5,7 @@
     <title>welcome</title>
 </head>
 <body>
-<h1>${message}</h1>
+<h1>JSP Sample</h1>
+<h2>${message}</h2>
 </body>
 </html>
